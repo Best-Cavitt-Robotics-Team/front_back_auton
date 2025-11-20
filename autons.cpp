15836e15a9c8a67f1c280da8177ce16e1ccaf01a
Best-Cavitt-Robotics-Team <bestcavittvexteam@gmail.com>
2025-11-19 16:16:48 -0800

    Add files via upload

--- autons.cpp
+++ autons.cpp
@@ -5,32 +5,31 @@
 void autonomous() {
     switch (selected_auton){
         case 0:
-        translate(24,0,750);
+        translate(25,0,750); //prev 24
         pros::delay(400);
         setHeading(0,70,500);
         pros::delay(250);
         intake_bottom.move_velocity(-600);
         pros::delay(100);
-        translate(20,70,750); //tune
-        pros::delay(500); //tune
-        intake_bottom.move_velocity(0);
-        setHeading(0,253,750);
+        translate(20,70,750); 
+        pros::delay(500); 
+        setHeading(0,260,750); //prev 253
         pros::delay(400);
         ballblock.set_value(true);
-        translate(-17,253,1000); //tune
+        translate(-17,260,1000); //prev 253
         pros::delay(400);
         intake_top.move_velocity(-400);
         intake_bottom.move_velocity(-400);
         pros::delay(1750);
         intake_top.move_velocity(0);
         intake_bottom.move_velocity(0);
-        translate(68,257,1000);
+        translate(66,260,2000);
         pros::delay(400);
         ballblock.set_value(false);
         setHeading(0,220,1000);
         scraper1.set_value(true);
         scraper2.set_value(true);
-        translate(-15,215,500);
+        translate(-5,215,500);
         pros::delay(100);//maybe not
         intake_bottom.move_velocity(-600);
         translate(20,215,750);
